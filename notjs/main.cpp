e76870906aa57d2734801a510ebb15ee511a35d7
Artem Tyurin <artem.tyurin@uber.com>
2020-04-12 17:54:08 +0200

    Initial Commit

---
+++ notjs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  notjs
+//
+//  Created by Artem Tyurin on 4/12/20.
+//  Copyright © 2020 Artem Tyurin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
